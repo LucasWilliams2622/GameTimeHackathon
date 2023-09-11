ea30353fefcb53d286dc8dc4abce4d354e32f013
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-09-11 21:23:37 +0700

    done scene 1

---
+++ Assets/Script/NPC/NPXFollowing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPXFollowing : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Tính toán hướng di chuyển từ NPC đến nhân vật
+        //Vector3 direction = playerTransform.position - transform.position;
+        //direction.Normalize();
+
+        //transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+}
