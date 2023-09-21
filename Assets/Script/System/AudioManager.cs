a5bc627d0aed382f0c35a7c10e0837e368a8a81a
Nguyễn Văn Tú <nguyenvantugamingchannel@gmail.com>
2023-09-22 00:52:01 +0700

    asdasd

---
+++ Assets/Script/System/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public static float masterVolume = 1.0f; // Biến lưu trữ âm lượng chung
+
+    // Phương thức để thiết lập âm lượng chung
+    public static void SetMasterVolume(float volume)
+    {
+        masterVolume = volume;
+
+        // Điều chỉnh âm lượng của tất cả AudioSource trong scene
+        AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.volume = volume;
+        }
+    }
+}
