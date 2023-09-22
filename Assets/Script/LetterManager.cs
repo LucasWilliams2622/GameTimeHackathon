d4faf7c3eaac3ec9533b7aaf81eb1ba491773a7d
Nguyễn Văn Tú <nguyenvantugamingchannel@gmail.com>
2023-09-22 11:02:38 +0700

    asn

--- Assets/Script/LetterManager.cs
+++ Assets/Script/LetterManager.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 
 public class LetterManager : MonoBehaviour
 {
-    public GameObject pressE;
     public GameObject canvas;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         canvas.SetActive(true);
     }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        pressE.SetActive(false);
-    }
-
 }
